cf5042b0442578853de3dda3c260ad32d1e3dce0
Kirmann Denis <kirmanndenis@gmail.com>
2021-07-30 16:08:52 +0200

    ajout des tags en dinamique dans des li

--- index.js
+++ index.js
@@ -1,64 +1,71 @@
-
-
-
 fetch("./index.json")
   .then(function (resp) {
     return resp.json();
   })
   .then(function (data) {
     for (i = 0; i < data.photographers.length; i++) {
-        const section = document.querySelector(".photographers__cards");
+      const section = document.querySelector(".photographers__cards");
 
-        const photographersThumbnail1 = document.createElement("div");
-        photographersThumbnail1.classList.add("photographers__thumbnail");
-        section.appendChild(photographersThumbnail1);
-        
-        const link = document.createElement("a");
-        
-        photographersThumbnail1.appendChild(link);
-        
-        const divPhoto = document.createElement("div");
-        
-        link.appendChild(divPhoto);
-        
-        let img = document.createElement("img");
-        
-        divPhoto.appendChild(img);
-        
-        let h2 = document.createElement("h2");
-        
-        link.appendChild(h2);
-        
-        const photographersDescription = document.createElement("div");
-        photographersDescription.classList.add("photographers__description");
-        
-        photographersThumbnail1.appendChild(photographersDescription);
-        
-        const h3 = document.createElement("h3");
-        
-        photographersDescription.appendChild(h3);
-        
-        const h4 = document.createElement("h4");
-        
-        photographersDescription.appendChild(h4);
-        
-        const p = document.createElement("p");
-        
-        photographersDescription.appendChild(p);
-        
-        const ul = document.createElement("ul");
-        
-        photographersThumbnail1.appendChild(ul);
+      const photographersThumbnail1 = document.createElement("div");
+      photographersThumbnail1.classList.add("photographers__thumbnail");
+      section.appendChild(photographersThumbnail1);
+
+      const link = document.createElement("a");
+      link.setAttribute("href", "#");
+
+      photographersThumbnail1.appendChild(link);
+
+      const divPhoto = document.createElement("div");
+
+      link.appendChild(divPhoto);
+
+      let img = document.createElement("img");
+
+      divPhoto.appendChild(img);
+
+      let h2 = document.createElement("h2");
+
+      link.appendChild(h2);
+
+      const photographersDescription = document.createElement("div");
+      photographersDescription.classList.add("photographers__description");
+
+      photographersThumbnail1.appendChild(photographersDescription);
+
+      const h3 = document.createElement("h3");
+
+      photographersDescription.appendChild(h3);
+
+      const h4 = document.createElement("h4");
+
+      photographersDescription.appendChild(h4);
+
+      const p = document.createElement("p");
+
+      photographersDescription.appendChild(p);
+
+      const ul = document.createElement("ul");
+
+      photographersThumbnail1.appendChild(ul);
+
+      img.setAttribute(
+        "src",
+        "./img/Sample Photos/" +
+          data.photographers[i].name +
+          "/" +
+          data.photographers[i].portrait
+      );
+      h2.innerHTML = data.photographers[i].name;
+      h3.innerHTML =
+        data.photographers[i].city + "," + data.photographers[i].country;
+      h4.innerHTML = data.photographers[i].tagline;
+      p.innerHTML = data.photographers[i].price + "/jour";
+      // li.innerHTML = data.photographers[i].tags;
+      data.photographers[i].tags.forEach((element) => {
         let li = document.createElement("li");
-        
+        li.innerHTML = "#"+element;
         ul.appendChild(li);
-        img.setAttribute("src", data.photographers[i].portrait);
-        h2.innerHTML = data.photographers[i].name;
-        h3.innerHTML =
-          data.photographers[i].city + "," + data.photographers[i].country;
-        h4.innerHTML = data.photographers[i].tagline;
-        p.innerHTML = data.photographers[i].price + "/jour";
-        li.innerHTML = data.photographers[i].tags;
+      });
       console.log(data.photographers[i]);
     }
 
