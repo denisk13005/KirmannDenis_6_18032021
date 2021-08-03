61834c8de2d2399fa85908bf400fab1586db48e8
kirmann <k.dkle@hotmail.fr>
2021-08-03 21:01:41 +0200

    essai d'import de fetch dans le js

--- js/index.js
+++ js/index.js
@@ -1,101 +1,106 @@
-//apparition du boutton au scroll
-const btnScroll = document.querySelector(".mainRedirection");
-window.addEventListener("scroll", (e) => {
-  if (window.scrollY >= 220) {
-    btnScroll.classList.add("mainRedirectionVisible");
-  } else {
-    btnScroll.classList.remove("mainRedirectionVisible");
-  }
-});
+import {getData} from './utils';
+getData(); 
 
-//tri au clic sur un tag
+// //apparition du boutton au scroll
+// const btnScroll = document.querySelector(".mainRedirection");
+// window.addEventListener("scroll", (e) => {
+//   if (window.scrollY >= 220) {
+//     btnScroll.classList.add("mainRedirectionVisible");
+//   } else {
+//     btnScroll.classList.remove("mainRedirectionVisible");
+//   }
+// });
 
-const tags = document.querySelectorAll(".tag>ul>li");
-console.log(tags);
-let tagSelected = [];
-tags.forEach((tag) => {
-  tag.addEventListener("click", () => {
-    if (tagSelected.includes(tag.textContent)) {
-      tagSelected.pop(tag.textContent);
-    } else {
-      tagSelected.push(tag.textContent);
-    }
-    console.log(tagSelected);
-  });
-});
-const section = document.querySelector(".photographers__cards");
+// //tri au clic sur un tag
 
-//utilisation de l'api fetch pour la récupération des données json
-fetch("./index.json")
-  .then(function (resp) {
-    return resp.json();
-  })
-  .then(function (data) {
-    for (i = 0; i < data.photographers.length; i++) {
-      // création de la balise div vignette photographe
-      const photographersThumbnail1 = document.createElement("div");
-      photographersThumbnail1.classList.add("photographers__thumbnail");
-      section.appendChild(photographersThumbnail1);
+// const tags = document.querySelectorAll(".tag>ul>li");
+// console.log(tags);
+// let tagSelected = [];
+// tags.forEach((tag) => {
+//   tag.addEventListener("click", () => {
+//     if (tagSelected.includes(tag.textContent)) {
+//       tagSelected.pop(tag.textContent);
+//     } else {
+//       tagSelected.push(tag.textContent);
+//     }
+//     console.log(tagSelected);
+//   });
+// });
+// // const section = document.querySelector(".photographers__cards");
 
-      // création du lien pour photo et nom
-      const link = document.createElement("a");
-      link.setAttribute("href", "#");
-      photographersThumbnail1.appendChild(link);
 
-      // création de la balise div conteneur de la photo
-      const divPhoto = document.createElement("div");
-      link.appendChild(divPhoto);
 
-      // création de la balise img
-      let img = document.createElement("img");
-      divPhoto.appendChild(img);
+// // //utilisation de l'api fetch pour la récupération des données json
+// // fetch("./index.json")
+// //   .then(function (resp) {
+// //     return resp.json();
+// //   })
+// //   .then(function (data) {
+// //     for (i = 0; i < data.photographers.length; i++) {
+// //       // création de la balise div vignette photographe
+// //       const photographersThumbnail1 = document.createElement("div");
+// //       photographersThumbnail1.classList.add("photographers__thumbnail");
+// //       section.appendChild(photographersThumbnail1);
 
-      //création de la balise h2 nom du photographe
-      let h2 = document.createElement("h2");
-      link.appendChild(h2);
+// //       // création du lien pour photo et nom
+// //       const link = document.createElement("a");
+// //       link.setAttribute("href", "#");
+// //       photographersThumbnail1.appendChild(link);
 
-      //création de la balise div pour la description du photographe
-      const photographersDescription = document.createElement("div");
-      photographersDescription.classList.add("photographers__description");
-      photographersThumbnail1.appendChild(photographersDescription);
+// //       // création de la balise div conteneur de la photo
+// //       const divPhoto = document.createElement("div");
+// //       link.appendChild(divPhoto);
 
-      //création de la balise h3 ville du photographe
-      const h3 = document.createElement("h3");
-      photographersDescription.appendChild(h3);
+// //       // création de la balise img
+// //       let img = document.createElement("img");
+// //       divPhoto.appendChild(img);
 
-      //création de la balise h4 pour la citation du photographe
-      const h4 = document.createElement("h4");
-      photographersDescription.appendChild(h4);
+// //       //création de la balise h2 nom du photographe
+// //       let h2 = document.createElement("h2");
+// //       link.appendChild(h2);
 
-      //crétaion de la balise p pour le prix
-      const p = document.createElement("p");
-      photographersDescription.appendChild(p);
+// //       //création de la balise div pour la description du photographe
+// //       const photographersDescription = document.createElement("div");
+// //       photographersDescription.classList.add("photographers__description");
+// //       photographersThumbnail1.appendChild(photographersDescription);
 
-      // création de la balise ul qui comprendra les li correspondants aux tags
-      const ul = document.createElement("ul");
-      photographersThumbnail1.appendChild(ul);
-      //injection de l'attribut source à la balise img avec un codage dynamique de la valeur
-      img.setAttribute(
-        "src",
-        "./img/Sample Photos/" +
-          data.photographers[i].name +
-          "/" +
-          data.photographers[i].portrait
-      );
-      //injection du nom du photographe dans la balise h2
-      h2.innerHTML = data.photographers[i].name;
-      //injection de la localisation du photographe
-      h3.innerHTML =
-        data.photographers[i].city + "," + data.photographers[i].country;
-      //injection de la citation
-      h4.innerHTML = data.photographers[i].tagline;
-      //injection du prix
-      p.innerHTML = data.photographers[i].price + "/jour";
-      //crétaion de li correspondant aux tags
-      data.photographers[i].tags.forEach((element) => {
-        let li = document.createElement("li");
-        li.innerHTML = "#" + element;
-        ul.appendChild(li);
-      });
-    }
-  });
+// //       //création de la balise h3 ville du photographe
+// //       const h3 = document.createElement("h3");
+// //       photographersDescription.appendChild(h3);
+
+// //       //création de la balise h4 pour la citation du photographe
+// //       const h4 = document.createElement("h4");
+// //       photographersDescription.appendChild(h4);
+
+// //       //crétaion de la balise p pour le prix
+// //       const p = document.createElement("p");
+// //       photographersDescription.appendChild(p);
+
+// //       // création de la balise ul qui comprendra les li correspondants aux tags
+// //       const ul = document.createElement("ul");
+// //       photographersThumbnail1.appendChild(ul);
+// //       //injection de l'attribut source à la balise img avec un codage dynamique de la valeur
+// //       img.setAttribute(
+// //         "src",
+// //         "./img/Sample Photos/" +
+// //           data.photographers[i].name +
+// //           "/" +
+// //           data.photographers[i].portrait
+// //       );
+// //       //injection du nom du photographe dans la balise h2
+// //       h2.innerHTML = data.photographers[i].name;
+// //       //injection de la localisation du photographe
+// //       h3.innerHTML =
+// //         data.photographers[i].city + "," + data.photographers[i].country;
+// //       //injection de la citation
+// //       h4.innerHTML = data.photographers[i].tagline;
+// //       //injection du prix
+// //       p.innerHTML = data.photographers[i].price + "/jour";
+// //       //crétaion de li correspondant aux tags
+// //       data.photographers[i].tags.forEach((element) => {
+// //         let li = document.createElement("li");
+// //         li.innerHTML = "#" + element;
+// //         ul.appendChild(li);
+// //       });
+// //     }
+// //   });
---
+++ js/utils.js
@@ -0,0 +1,14 @@
+// export async function getData(){
+//   let resp = await fetch("index.json");
+//   let response = await resp.json();  
+//   try{
+//   console.log(response.photographers);
+//   return response;
+//   } catch(error){
+//     console.log(error);
+//   }
+// };
+let getData = ()=>{
+  console.log("test");
+}
+export  { getData}
