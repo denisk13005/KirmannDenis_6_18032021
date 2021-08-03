019e440995567472d1730646b75cd8b364c3f5e1
kirmann <k.dkle@hotmail.fr>
2021-08-03 21:02:38 +0200

    reorg

--- js/utils.js
+++ js/utils.js
@@ -1,14 +1,12 @@
-// export async function getData(){
-//   let resp = await fetch("index.json");
-//   let response = await resp.json();  
-//   try{
-//   console.log(response.photographers);
-//   return response;
-//   } catch(error){
-//     console.log(error);
-//   }
-// };
-let getData = ()=>{
-  console.log("test");
-}
-export  { getData}
+async function getData(){
+  let resp = await fetch("index.json");
+  let response = await resp.json();  
+  try{
+  console.log(response.photographers);
+  return response;
+  } catch(error){
+    console.log(error);
+  }
+};
+
+export  { getData};
