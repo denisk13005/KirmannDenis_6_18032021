65cad21dfa3814ff803034403518a094a4be38c5
kirmann <k.dkle@hotmail.fr>
2021-08-06 21:07:37 +0200

    creation de la classe photographe et de la methode render dans photographers.js

--- js/photographers.js
+++ js/photographers.js
@@ -1,37 +1,113 @@
-// class Photographer {
-//   constructor(name,id, city,country,tags, tagline,price,portrait){
-//     this.name = name,
-//     this.id = id,
-//     this.city = city,
-//     this.country = country,
-//     this.tags = tags,
-//     this.tagline = tagline,
-//     this.price = price,
-//     this.portrait = portrait
-//   }
-//   render(){    
-//       console.log(this.name);
-    
-//   }
-// }
-// let photo = new Photographer("mimi")
-// console.log(photo)
+let section = document.querySelector(".photographers__cards");
 
-const person = {
-  name: 'Wes',
-  job: 'Web Developer',
-  city: 'Hamilton',
-  bio: 'Wes is a really cool guy that loves to teach web development!'
+let photo = [
+  {
+    "name": "Mimi Keel",
+    "id": 243,
+    "city": "London",
+    "country": "UK",
+    "tags": ["portrait", "events", "travel", "animals"],
+    "tagline": "Voir le beau dans le quotidien",
+    "price": 400,
+    "portrait": "Mimikeel.jpg"
+  },
+  {
+    "name": "Ellie-Rose Wilkens",
+    "id": 930,
+    "city": "Paris",
+    "country": "France",
+    "tags": ["sports", "architecture"],
+    "tagline": "Capturer des compositions complexes",
+    "price": 250,
+    "portrait": "EllieRoseWilkens.jpg"
+  },
+  {
+    "name": "Tracy Galindo",
+    "id": 82,
+    "city": "Montreal",
+    "country": "Canada",
+    "tags": ["art", "fashion", "events"],
+    "tagline": "Photographe freelance",
+    "price": 500,
+    "portrait": "TracyGalindo.jpg"
+  },
+  {
+    "name": "Nabeel Bradford",
+    "id": 527,
+    "city": "Mexico City",
+    "country": "Mexico",
+    "tags": ["travel", "portrait"],
+    "tagline": "Toujours aller de l'avant",
+    "price": 350,
+    "portrait": "NabeelBradford.jpg"
+  },
+  {
+    "name": "Rhode Dubois",
+    "id": 925,
+    "city": "Barcelona",
+    "country": "Spain",
+    "tags": ["sport", "fashion", "events", "animals"],
+    "tagline": "Je crée des souvenirs",
+    "price": 275,
+    "portrait": "RhodeDubois.jpg"
+  },
+  {
+    "name": "Marcel Nikolic",
+    "id": 195,
+    "city": "Berlin",
+    "country": "Germany",
+    "tags": ["travel", "architecture"],
+    "tagline": "Toujours à la recherche de LA photo",
+    "price": 300,
+    "portrait": "MarcelNikolic.jpg"
+  }
+
+]  
+
+      
+    
+  
+class Photographer {
+  constructor(name,id, city,country,tags, tagline,price,portrait){
+    this.name = name,
+    this.id = id,
+    this.city = city,
+    this.country = country,
+    this.tags = tags,
+    this.tagline = tagline,
+    this.price = price,
+    this.portrait = portrait
+  }
+  render(){
+    console.log("ca marche");
+    let tags = this.tags;  
+      let div = `       
+        <div class="photographers__thumbnail">
+         <a href="#">
+           <div>
+             <img src="./img/Sample Photos/${this.name}/${
+        this.portrait
+      }" alt="photo de ${this.name}" />
+           </div>
+           <h2>${this.name}</h2>
+         </a>
+         <div class="photographers__description">
+           <h3>${this.city}, ${this.country}</h3>
+           <h4>${this.tagline}</h4>
+           <p>${this.price}/jour</p>
+         </div>
+         <ul>
+            ${tags.map((tag) => `<li>#${tag}`).join("")}
+         </ul>
+        
+          `;
+      section.innerHTML += div;
+    }
+  
 }
 
-// And then create our markup:
-const markup = `
-<div class="person">
-  <h2>
-      ${person.name}
-  </h2>
-  <p class="location">${person.city}</p>
-  <p class="bio">${person.bio}</p>
-</div>
-`;
-console.log(markup)
+photo.forEach(element => {
+  let photographersSelected = new Photographer(element.name, element.id, element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
+  photographersSelected.render()
+  console.log(photographersSelected.id);
+});
