f835639d09d0dc647d0faf4b38e7b714c9b1e1fe
kirmann <k.dkle@hotmail.fr>
2021-08-08 17:44:07 +0200

    essai d ajout de classlist disploay none au click sur un tag

--- js/index.js
+++ js/index.js
@@ -2,23 +2,12 @@ import { getDataPhotographers } from "./utils.js";
 
 let section = document.querySelector(".photographers__cards");
 let data = await getDataPhotographers("index.json"); //récupération des données json
-let photo = data.photographers;
-let media = data.media
-console.log(media)
-//test de mise en place d'une fonction de tri des photographes au click
-let tab = ["animals", "events", "portrait"];
-let tagsArray = photo[0].tags;
-let tabtest = [];
-for (let i = 0; i < tab.length; i++) {
-  if (tagsArray.includes(tab[i])) {
-    console.log(photo[i]);
-    tabtest.push(photo[i]);
-    break;
-  }
-}
-console.log(tab);
+let photographers = data.photographers; //récupération des données liées aux photographes
+let media = data.media//récupération des données liées aux medias
+let photographersSelected = [];
 
-// création de la classe photographe
+
+// ******************************************création de la classe photographe
 class Photographer {
   constructor(name, id, city, country, tags, tagline, price, portrait) {
       this.name = name,
@@ -30,9 +19,8 @@ class Photographer {
       this.price = price,
       this.portrait = portrait;
   }
+  //******************************************création de la méthode render qui permettra d'afficher les photographes
   render() {
-    //création de la méthode render qui permettra d'afficher les photographes
-    let tags = this.tags;
     let div = `       
         <div class="photographers__thumbnail">
          <a href="#">
@@ -47,60 +35,75 @@ class Photographer {
            <p>${this.price}/jour</p>
          </div>
          <ul>
-            ${tags.map((tag) => `<li>#${tag}`).join("")}
+            ${this.tags.map((tag) => `<li>#${tag}`).join("")}
          </ul>
         
           `;
     section.innerHTML += div;
   }
 }
-//tri au clic sur un tag
+
+
+//**********************************************affichage des photographes 
+photographers.forEach((element) => {
+  let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
+  photographer.render()})
+
+
+
+
+//**********************************************création d'un tableau contanant les tags sélectionnés
 
 const tags = document.querySelectorAll(".tag>ul>li");
 let tagSelected = [];
+let tagDesired ;
 tags.forEach((tag) => {
-  tag.addEventListener("click", () => {
+  tag.addEventListener("click", () => { 
+      console.log(photographersSelected)    
+      photographers.forEach((element) => {
+        let divThumbnail = document.querySelectorAll(".photographers__thumbnail");
+        divThumbnail.forEach(elmt=>{
+          elmt.classList.add("displayNone");
+        })
+      })     
+        
+    
     if (tagSelected.includes(tag.className)) {
       const index = tagSelected.indexOf(tag.className);
       tagSelected.splice(index, 1);
     } else {
       tagSelected.push(tag.className);
+      tagSelected.forEach(element => {
+        tagDesired = element;
+        return tagDesired
+      })
+      
+      photographers.forEach(element => {
+        if(element.tags.includes(tagDesired)){
+          photographersSelected.push(element)
+          
+
+        }
+      });
+      
     }
-    console.log(tagSelected);
+    
   });
 });
+console.log(tagSelected)
 
-if (tagSelected.length === 0) {
-  photo.forEach((element) => {
-    let photographer = new Photographer(
-      element.name,
-      element.id,
-      element.city,
-      element.country,
-      element.tags,
-      element.tagline,
-      element.price,
-      element.portrait
-    );
-    photographer.render();
-  });
-} else {
-  tagSelected.forEach((element) => {
-    let photographer = new Photographer(
-      element.name,
-      element.id,
-      element.city,
-      element.country,
-      element.tags,
-      element.tagline,
-      element.price,
-      element.portrait
-    );
-    photographer.render();
-  });
-}
+//**********************************************création d'un tableau contenant les photographes correspondants au tags sélectionnés
 
-//apparition du boutton au scroll
+
+
+
+
+
+
+
+
+//************************************************apparition du boutton au scroll
+
 const btnScroll = document.querySelector(".mainRedirection");
 window.addEventListener("scroll", (e) => {
   if (window.scrollY >= 220) {
