34cf1f728e500d99abc9f95be616c59e15500ce1
kirmann <k.dkle@hotmail.fr>
2021-08-12 19:53:58 +0200

    début de fonction tri au click sur tag

--- js/index.js
+++ js/index.js
@@ -1,98 +1,108 @@
 import { getDataPhotographers } from "./utils.js";
-
-let section = document.querySelector(".photographers__cards");
-let data = await getDataPhotographers("index.json"); //récupération des données json
-let photographers = data.photographers; //récupération des données liées aux photographes
-let media = data.media//récupération des données liées aux medias
-let photographersSelected = [];
+import { Photographer } from "./photographers.js";
 
 
-// ******************************************création de la classe photographe
-class Photographer {
-  constructor(name, id, city, country, tags, tagline, price, portrait) {
-      this.name = name,
-      this.id = id,
-      this.city = city,
-      this.country = country,
-      this.tags = tags,
-      this.tagline = tagline,
-      this.price = price,
-      this.portrait = portrait;
-  }
-  //******************************************création de la méthode render qui permettra d'afficher les photographes
-  render() {
-    let div = `       
-        <div class="photographers__thumbnail">
-         <a href="#">
-           <div>
-             <img src="./img/Sample Photos/${this.name}/${this.portrait}" alt="photo de ${this.name}" />
-           </div>
-           <h2>${this.name}</h2>
-         </a>
-         <div class="photographers__description">
-           <h3>${this.city}, ${this.country}</h3>
-           <h4>${this.tagline}</h4>
-           <p>${this.price}/jour</p>
-         </div>
-         <ul>
-            ${this.tags.map((tag) => `<li>#${tag}`).join("")}
-         </ul>
-        
-          `;
-    section.innerHTML += div;
-  }
-}
+const section = document.querySelector(".photographers__cards");
+const data = await getDataPhotographers("index.json"); //récupération des données json
+const photographersData = data.photographers; //récupération des données liées aux photographes
+const media = data.media//récupération des données liées aux medias
+let photographers = [];
+console.log(photographers)
+console.log(photographersData)
 
 
 //**********************************************affichage des photographes 
-photographers.forEach((element) => {
-  let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
-  photographer.render()})
+photographersData.forEach((element) => {
+  photographers.push( new Photographer(element)) ; 
+})
+photographers.forEach(element => {
+  section.innerHTML += element.render()
+})
+//********************************************recherche si le tag est présent et affiche les photographes qui ont le tag */
 
+const test = (tag) =>{
+    const qque = photographers.filter(x => x.hasTag(tag))
+    console.log(qque);
+    section.innerHTML ="";
+    qque.forEach(element => {
+      section.innerHTML += element.render()      
+    })
+console.log(qque)
 
-
+}
+let li = document.querySelectorAll(".tag>ul>li")
+li.forEach((element)=> element.addEventListener('click',(e)=>{
+  test(e.target.className)
+}))
+// li.addEventListener('click',(e)=>{
+//   console.log(e.target.className)
+// })
+// test("portrait")
+// const tabTagSearch = []
+// photographers.forEach(element => tabTagSearch.push(element.tags))
+// console.log(tabTagSearch)
 
 //**********************************************création d'un tableau contanant les tags sélectionnés
 
-const tags = document.querySelectorAll(".tag>ul>li");
-let tagSelected = [];
-let tagDesired ;
-tags.forEach((tag) => {
-  tag.addEventListener("click", () => { 
-      console.log(photographersSelected)    
-      photographers.forEach((element) => {
-        let divThumbnail = document.querySelectorAll(".photographers__thumbnail");
-        divThumbnail.forEach(elmt=>{
-          elmt.classList.add("displayNone");
-        })
-      })     
-        
-    
-    if (tagSelected.includes(tag.className)) {
-      const index = tagSelected.indexOf(tag.className);
-      tagSelected.splice(index, 1);
-    } else {
-      tagSelected.push(tag.className);
-      tagSelected.forEach(element => {
-        tagDesired = element;
-        return tagDesired
-      })
-      
-      photographers.forEach(element => {
-        if(element.tags.includes(tagDesired)){
-          photographersSelected.push(element)
-          
+// const tags = document.querySelectorAll(".tag>ul>li");
+// let tagSelected = [];
+// let tagDesired =[] ;
+// let photographersSelected = [];//tableau contenant les photographes sélectionnés en fonction du tag cliqué
+// tags.forEach((tag) => {
+//   tag.addEventListener("click", () => { 
 
-        }
-      });
+//     console.log(tag.dataset.name)
+//       //cacher tous les photographes
+//       // photographers.forEach((element) => {
+//       //     let divThumbnail = document.querySelectorAll(".photographers__thumbnail");
+//       //     divThumbnail.forEach(elmt=>{
+//       //       elmt.classList.add("displayNone");
+//       //     })
+//       //   })   
+//     //je vérifie si le tableau tagSelected contient le tag sélectionné, si oui je l'enlève si non je l'ajoute 
+//     if (tagSelected.includes(tag.className)) { 
+//       const index = tagSelected.indexOf(tag.className);
+//       tagSelected.splice(index, 1);
+//     } else {
+//       tagSelected.push(tag.className);
+//       tagSelected.forEach(element => {
+//         tagDesired = element;
+//       })
       
-    }
+//       photographers.forEach(element => {
+//         if(element.tags.includes(tagDesired)){
+//           photographersSelected.push(element)
+//         }
+//       });
+      
+      
+//     }
+//     if(tagSelected.length === 0){
+//        //si aucun tag n'est sélectionné afficher tous les photographes
+//       photographers.forEach((element) => {
+//         let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
+//         photographer.render()})     
+//         console.log("vrai")   
+//     }else{
+//       console.log("faux")
+//       photographersSelected.forEach((element) => {   
+//         console.log(element.tags)     
+//         //   
+//         let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
+//         photographer.render()})
+//         photographersSelected=[];
+//     }
     
-  });
-});
-console.log(tagSelected)
+//     console.log(tagSelected)
+//     console.log(photographersSelected)
+//     console.log(tagDesired)
 
-//**********************************************création d'un tableau contenant les photographes correspondants au tags sélectionnés
+    
+//   });
+// });
+
+
+
 
 
 
--- js/photographers.js
+++ js/photographers.js
@@ -1,74 +1,7 @@
-let section = document.querySelector(".photographers__cards");
 
-let photo = [
-  {
-    "name": "Mimi Keel",
-    "id": 243,
-    "city": "London",
-    "country": "UK",
-    "tags": ["portrait", "events", "travel", "animals"],
-    "tagline": "Voir le beau dans le quotidien",
-    "price": 400,
-    "portrait": "Mimikeel.jpg"
-  },
-  {
-    "name": "Ellie-Rose Wilkens",
-    "id": 930,
-    "city": "Paris",
-    "country": "France",
-    "tags": ["sports", "architecture"],
-    "tagline": "Capturer des compositions complexes",
-    "price": 250,
-    "portrait": "EllieRoseWilkens.jpg"
-  },
-  {
-    "name": "Tracy Galindo",
-    "id": 82,
-    "city": "Montreal",
-    "country": "Canada",
-    "tags": ["art", "fashion", "events"],
-    "tagline": "Photographe freelance",
-    "price": 500,
-    "portrait": "TracyGalindo.jpg"
-  },
-  {
-    "name": "Nabeel Bradford",
-    "id": 527,
-    "city": "Mexico City",
-    "country": "Mexico",
-    "tags": ["travel", "portrait"],
-    "tagline": "Toujours aller de l'avant",
-    "price": 350,
-    "portrait": "NabeelBradford.jpg"
-  },
-  {
-    "name": "Rhode Dubois",
-    "id": 925,
-    "city": "Barcelona",
-    "country": "Spain",
-    "tags": ["sport", "fashion", "events", "animals"],
-    "tagline": "Je crée des souvenirs",
-    "price": 275,
-    "portrait": "RhodeDubois.jpg"
-  },
-  {
-    "name": "Marcel Nikolic",
-    "id": 195,
-    "city": "Berlin",
-    "country": "Germany",
-    "tags": ["travel", "architecture"],
-    "tagline": "Toujours à la recherche de LA photo",
-    "price": 300,
-    "portrait": "MarcelNikolic.jpg"
-  }
 
-]  
-
-      
-    
-  
 class Photographer {
-  constructor(name,id, city,country,tags, tagline,price,portrait){
+  constructor({name,id, city,country,tags, tagline,price,portrait}){
     this.name = name,
     this.id = id,
     this.city = city,
@@ -79,15 +12,11 @@ class Photographer {
     this.portrait = portrait
   }
   render(){
-    console.log("ca marche");
-    let tags = this.tags;  
       let div = `       
         <div class="photographers__thumbnail">
          <a href="#">
            <div>
-             <img src="./img/Sample Photos/${this.name}/${
-        this.portrait
-      }" alt="photo de ${this.name}" />
+             <img src="./img/Sample Photos/${this.name}/${this.portrait}" alt="photo de ${this.name}" />
            </div>
            <h2>${this.name}</h2>
          </a>
@@ -97,17 +26,17 @@ class Photographer {
            <p>${this.price}/jour</p>
          </div>
          <ul>
-            ${tags.map((tag) => `<li>#${tag}`).join("")}
+            ${this.tags.map((tag) => `<li>#${tag}`).join("")}
          </ul>
-        
+        </div>
           `;
-      section.innerHTML += div;
+      return div
     }
+  hasTag(tag){
+    console.log("test")
+    return this.tags.includes(tag)
+  }
+  
   
 }
-
-photo.forEach(element => {
-  let photographersSelected = new Photographer(element.name, element.id, element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
-  photographersSelected.render()
-  console.log(photographersSelected.id);
-});
+export {Photographer} ;
