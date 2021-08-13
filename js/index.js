115e6630d48adbe7c5e930d3195923a52384f39c
kirmann <k.dkle@hotmail.fr>
2021-08-13 11:28:45 +0200

    suppression des li dans le html + finition de la fonction de tri

--- js/index.js
+++ js/index.js
@@ -37,85 +37,27 @@ photographers.forEach((element) => {
   section.innerHTML += element.render();
 });
 //********************************************recherche si le tag est présent et affiche les photographes qui ont le tag */
-
+let tagsDesired = [];
 const test = (tag) => {
   const qque = photographers.filter((x) => x.hasTag(tag));
-  console.log(qque);
   section.innerHTML = "";
   qque.forEach((element) => {
-    section.innerHTML += element.render();
+    tagsDesired.push(element);
   });
+  let tagsDesiredSet = new Set(tagsDesired);
+  
+  tagsDesiredSet.forEach((element)=>{
+    section.innerHTML += element.render();
+  })
   console.log(qque);
+  console.log(tagsDesired)
+
 };
 document.querySelectorAll(".tag>ul>li").forEach((element) =>
   element.addEventListener("click", (e) => {
     test(e.target.dataset.name);
   })
 );
-// li.addEventListener('click',(e)=>{
-//   console.log(e.target.className)
-// })
-// test("portrait")
-// const tabTagSearch = []
-// photographers.forEach(element => tabTagSearch.push(element.tags))
-// console.log(tabTagSearch)
-
-//**********************************************création d'un tableau contanant les tags sélectionnés
-
-// const tags = document.querySelectorAll(".tag>ul>li");
-// let tagSelected = [];
-// let tagDesired =[] ;
-// let photographersSelected = [];//tableau contenant les photographes sélectionnés en fonction du tag cliqué
-// tags.forEach((tag) => {
-//   tag.addEventListener("click", () => {
-
-//     console.log(tag.dataset.name)
-//       //cacher tous les photographes
-//       // photographers.forEach((element) => {
-//       //     let divThumbnail = document.querySelectorAll(".photographers__thumbnail");
-//       //     divThumbnail.forEach(elmt=>{
-//       //       elmt.classList.add("displayNone");
-//       //     })
-//       //   })
-//     //je vérifie si le tableau tagSelected contient le tag sélectionné, si oui je l'enlève si non je l'ajoute
-//     if (tagSelected.includes(tag.className)) {
-//       const index = tagSelected.indexOf(tag.className);
-//       tagSelected.splice(index, 1);
-//     } else {
-//       tagSelected.push(tag.className);
-//       tagSelected.forEach(element => {
-//         tagDesired = element;
-//       })
-
-//       photographers.forEach(element => {
-//         if(element.tags.includes(tagDesired)){
-//           photographersSelected.push(element)
-//         }
-//       });
-
-//     }
-//     if(tagSelected.length === 0){
-//        //si aucun tag n'est sélectionné afficher tous les photographes
-//       photographers.forEach((element) => {
-//         let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
-//         photographer.render()})
-//         console.log("vrai")
-//     }else{
-//       console.log("faux")
-//       photographersSelected.forEach((element) => {
-//         console.log(element.tags)
-//         //
-//         let photographer = new Photographer(element.name,element.id,element.city,element.country,element.tags,element.tagline,element.price,element.portrait);
-//         photographer.render()})
-//         photographersSelected=[];
-//     }
-
-//     console.log(tagSelected)
-//     console.log(photographersSelected)
-//     console.log(tagDesired)
-
-//   });
-// });
 
 //************************************************apparition du boutton au scroll
 
