e65dbd84d7db02ac0da85b0a8ce9dc5b06a96d7c
kirmann <k.dkle@hotmail.fr>
2021-08-31 18:42:11 +0200

    restructuration du code

--- js/media.js
+++ js/media.js
@@ -1,35 +1,79 @@
-import { getDataPhotographers } from './utils.js'
-
-class Media {
-  constructor({ title, tagline, likes , photographerId}) {
-      this.title = title;
-      this.tagline = tagline;      
-      this.likes = likes;
-      this.photographerId = photographerId
+// ***************************************création de la classe qui décriera le photographe sélectionné
+export class PhotographerInfo {
+  constructor({ name, id, city, country, tags, tagline, portrait, price }) {
+    this.name = name
+    this.id = id
+    this.city = city
+    this.country = country
+    this.tags = tags
+    this.tagline = tagline
+    this.portrait = portrait
+    this.price = price
   }
 
+  render() {
+    const div = `
+        <div class ='infoAndButton'>
+          <div class="photographer__informations">
+            <h1>${this.name}</h1>
+            <h2>${this.city},${this.country}</h2>
+            <h3>${this.tagline}</h3>
+            <nav>
+              <ul>
+              ${this.tags.map((tag) => `<li>#${tag}`).join('')}
+              </ul>
+            </nav>
+          </div> 
+          <button>Contactez-moi</button> 
+        </div>
+        <div class="photo">
+          <img src="../img/Sample Photos/${this.id}/${this.portrait}" alt="" />
+        </div> 
+        <div class='likeCountResume'>  
+          <div class='total__likes'>        
+          <p>compte des likes</p>
+          <img src='../img/heart-solid-black.svg'/>
+          </div>
+
+          <p>${this.price}€/jour</p>
+        
+        </div>
+      
+        `
+    return div
+  }
 }
-//* *******************classe Photo avec sa méthode générateImage pour créer une vignette correspondant à la photo */
-class Photo extends Media{  
-  constructor({ title, tagline, image, likes , photographerId}) {
-      super({ title, tagline, likes , photographerId});    
-      this.image = image;      
+
+// **********************************************************création de la super classe media
+export class Media {
+  constructor({ title, tagline, likes, photographerId }) {
+    this.title = title
+    this.tagline = tagline
+    this.likes = likes
+    this.photographerId = photographerId
+  }
+}
+//* *******************classe Photo avec sa méthode render pour créer une vignette correspondant à la photo */
+export class Photo extends Media {
+  constructor({ title, tagline, image, likes, photographerId }) {
+    super({ title, tagline, likes, photographerId })
+    this.image = image
   }
 
   render() {
     const div = `
     <div class="thumbnail">
           <div class="img__thumbnail">   
-    <img src="../img/Sample Photos/${this.photographerId}/${this.image}" alt="" />
+    <img src="../img/Sample Photos/${this.photographerId}/${this.image}" alt="" />      
           </div>
           <div class="thumbnail__description">
             <p>${this.title}</p>
             <div class="like__count">
               <p>${this.likes}</p>
               <img src="../img/heart-solid.svg" alt="" />
-            </div>
-          </div>
-    </div> 
+            </div>  
+          </div>  
+    </div>       
   
     `
     return div
@@ -37,76 +81,30 @@ class Photo extends Media{
 }
 
 //* ************************création de la classe Mp4 pour créer une vignette vidéo */
-class Mp4 extends Media{
-  constructor({ title, tagline, video, likes,photographerId }) {
-    super({ title, tagline, likes,photographerId })   
-      this.video = video;  
+export class Mp4 extends Media {
+  constructor({ title, tagline, video, likes, photographerId }) {
+    super({ title, tagline, likes, photographerId })
+    this.video = video
   }
 
   render() {
-   const div = `
+    const div = ` 
     <div class="thumbnail">
           <div class="img__thumbnail">
             <video
               src="../img/Sample Photos/${this.photographerId}/${this.video}"
               type="video/mp4"
-            ></video>
-          </div>
+            ></video>  
+          </div>  
           <div class="thumbnail__description">
             <p>${this.title}</p>
             <div class="like__count">
               <p>${this.likes}</p>
               <img src="../img/heart-solid.svg" alt="" " alt="" />
-            </div>
-          </div>
-    </div>
+            </div>  
+          </div>  
+    </div>      
     `
     return div
   }
 }
-
-
-
-const sectionThumbnail = document.querySelector('.container__thumbnail')
-const mediaToRender = []
-async function getMedia() {
-  const data = await getDataPhotographers('../index.json')
-  const position = window.location.href.indexOf('?')
-  const idphoto = window.location.href.substr(position + 1)
-  const media = data.media
-  const photographer = data.photographers
-  photographer.forEach(element=>{    
-    if(element.id === parseInt(idphoto)){
-      console.log(element);
-    }
-  })
-  
-  media.forEach((element) => {
-    if (element.photographerId === parseInt(idphoto)) {
-      mediaToRender.push(element)
-    }
-  })
-
-  mediaToRender.forEach((element) => {
-    // eslint-disable-next-line no-prototype-builtins
-    if (element.hasOwnProperty('image')) {
-      const thumb = new Photo(element)
-      const bc = thumb.render()
-      sectionThumbnail.innerHTML+= bc
-      
-    }
-  })
-  const vid = mediaToRender.filter((el) => {
-    return el.video
-    
-  })
-  console.log(vid);
-  vid.forEach(element => {
-    const videoThumbnail =new Mp4(element)
-    const ab = videoThumbnail.render()
-    sectionThumbnail.innerHTML+= ab
-  });
- 
-
-}
-getMedia()
--- js/photographers.js
+++ js/photographers.js
@@ -1,4 +1,5 @@
-import { getDataPhotographers } from './utils.js'
+import { getDataPhotographers } from './utils.js';
+import {Photo,Mp4,PhotographerInfo} from './media.js';
 
 // console.log(photographers);
 const data = await getDataPhotographers('../index.json')
@@ -15,96 +16,14 @@ data.photographers.forEach((element) => {
 
 const sectionInfo = document.querySelector('.photographer__description')
 
-// function createAPhotographer({
-//   name,
-//   id,
-//   city,
-//   country,
-//   tagline,
-//   tags,
-//   portrait
-// }) {
-//   return {
-//     name,
-//     id,
-//     city,
-//     country,
-//     tagline,
-//     tags,
-//     portrait,
-//     generateInfo
-//   }
-//   function generateInfo() {
-//     sectionInfo.innerHTML = `
-//     <div class ='infoAndButton'>
-//       <div class="photographer__informations">
-//         <h1>${name}</h1>
-//         <h2>${city},${country}</h2>
-//         <h3>${tagline}</h3>
-//         <nav>
-//           <ul>
-//           ${tags.map((tag) => `<li>#${tag}`).join('')}
-//           </ul>
-//         </nav>
-//       </div>
-//       <button>Contactez-moi</button>
-//     </div>
-//     <div class="photo">
-//       <img src="../img/Sample Photos/${name}/${portrait}" alt="" />
-//     </div>
-
-//     `
-//   }
-// }
-class PhotographerInfo {
-  constructor({ name, id, city, country, tags, tagline, portrait, price }) {
-    ;(this.name = name),
-      (this.id = id),
-      (this.city = city),
-      (this.country = country),
-      (this.tags = tags),
-      (this.tagline = tagline),
-      (this.portrait = portrait),
-      this.price=price
-  }
-  generateInfo() {
-    sectionInfo.innerHTML = `
-        <div class ='infoAndButton'>
-          <div class="photographer__informations">
-            <h1>${this.name}</h1>
-            <h2>${this.city},${this.country}</h2>
-            <h3>${this.tagline}</h3>
-            <nav>
-              <ul>
-              ${this.tags.map((tag) => `<li>#${tag}`).join('')}
-              </ul>
-            </nav>
-          </div> 
-          <button>Contactez-moi</button> 
-        </div>
-        <div class="photo">
-          <img src="../img/Sample Photos/${this.id}/${this.portrait}" alt="" />
-        </div> 
-        <div class='likeCountResume'>  
-          <div class='total__likes'>        
-          <p>compte des likes</p>
-          <img src='../img/heart-solid-black.svg'/>
-          </div>
-
-          <p>${this.price}€/jour</p>
-        
-        </div>
-      
-        `
-  }
-}
 
 let position = window.location.href.indexOf('?')
 let idphoto = window.location.href.substr(position + 1)
 photographers.forEach((element) => {
   if (element.id == idphoto) {
     const photographerInfos = new PhotographerInfo(element)
-    photographerInfos.generateInfo()
+    const photographe=photographerInfos.render()
+    sectionInfo.innerHTML += photographe
   }
 })
 
@@ -119,3 +38,49 @@ arrow.addEventListener('click', () => {
     element.classList.toggle('active')
   })
 })
+
+
+
+const sectionThumbnail = document.querySelector('.container__thumbnail')
+const mediaToRender = []
+async function getMedia() {
+  const data = await getDataPhotographers('../index.json')
+  const position = window.location.href.indexOf('?')
+  const idphoto = window.location.href.substr(position + 1)
+  const media = data.media
+  const photographer = data.photographers
+  photographer.forEach(element=>{    
+    if(element.id === parseInt(idphoto)){
+      console.log(element);
+    }
+  })
+  
+  media.forEach((element) => {
+    if (element.photographerId === parseInt(idphoto)) {
+      mediaToRender.push(element)
+    }
+  })
+
+  mediaToRender.forEach((element) => {
+    // eslint-disable-next-line no-prototype-builtins
+    if (element.hasOwnProperty('image')) {
+      const thumb = new Photo(element)
+      const bc = thumb.render()
+      sectionThumbnail.innerHTML+= bc
+      
+    }
+  })
+  const vid = mediaToRender.filter((el) => {
+    return el.video
+    
+  })
+  console.log(vid);
+  vid.forEach(element => {
+    const videoThumbnail =new Mp4(element)
+    const ab = videoThumbnail.render()
+    sectionThumbnail.innerHTML+= ab
+  });
+ 
+
+}
+getMedia()
