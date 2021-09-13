45027ed2e22442883b6d46624a541cc74883f468
kirmann <k.dkle@hotmail.fr>
2021-09-13 14:50:38 +0200

    mise en forme définitive de la modale plus message de validation

--- js/lightbox.js
+++ js/lightbox.js
@@ -12,35 +12,44 @@ export class Lightbox {
   render () {
     this.media.forEach((element, index) =>
       element.addEventListener('click', (e) => {
-        console.log(index)
+        const light = document.createElement('div')
+        light.classList.add('lightbox')
         const path = element.children[0].getAttribute('src')
         const title = element.children[0].getAttribute('alt')
         const type = element.children[0].localName
-        const light = new LightboxMedia(path, title, index, type)
-        this.main.innerHTML += light.render()
-        const nextMedia = document.querySelector('.lightbox__next')
-
-        nextMedia.addEventListener('click', () => {
-          const lightNext = new Lightbox()
-          this.main.innerHTML += lightNext.next()
+        const icone = new LightboxMedia(path, title, index, type)
+        light.innerHTML = icone.render()
+        this.main.appendChild(light)
+        // fermeture de la lightbox
+        const closeIcone = document.querySelector('.lightbox__close')
+        closeIcone.addEventListener('click', () => this.main.removeChild(light))
+        // changement de média
+        const arrowNext = document.querySelectorAll('.lightbox__next')
+        console.log(arrowNext)
+        arrowNext.forEach(arrow => {
+          arrow.addEventListener('click', () => {
+            console.log(arrow)
+            console.log(index)
+            const indexNext = index + 1
+            this.main.removeChild(light)
+            const lightNext = document.createElement('section')
+            lightNext.classList.add('lightbox')
+            const path = this.media[indexNext].children[0].getAttribute('src')
+            const title = this.media[indexNext].children[0].getAttribute('alt')
+            const type = this.media[indexNext].children[0].localName
+            const icone = new LightboxMedia(path, title, index, type)
+            lightNext.innerHTML = icone.render()
+            this.main.appendChild(lightNext)
+            index++
+            console.log(index)
+            if (index === 9) {
+              index = 0
+            }
+          })
         })
       })
 
     )
-  }
-
-  next () {
-    this.media.forEach((element, index) => {
-      const indexNext = index + 1
-      console.log(indexNext)
-      const path = this.media[indexNext].children[0].getAttribute('src')
-      const title = this.media[indexNext].children[0].getAttribute('alt')
-      const type = this.media[indexNext].children[0].localName
-      const light = new LightboxMedia(path, title, this.index, type)
-      this.main.innerHTML += light.render()
-      console.log(indexNext)
-      this.index++
-    })
   }
 }
 
@@ -62,12 +71,12 @@ export class LightboxMedia {
     const children = this.type === 'video' ? this.renderVideo() : this.renderImage()
 
     const div = `
-    <div class="lightbox">
+   
         <button class="lightbox__close"></button>
         <button class="lightbox__next"></button>
         <button class="lightbox__prev"></button>
         ${children}
-    </div>
+    
     `
     return div
   }
--- js/modale.js
+++ js/modale.js
@@ -29,9 +29,27 @@ export function contactPhotographer (nameOfPhotographerId) {
     closeModal.addEventListener('click', () => {
       main.removeChild(form)
     })
-    const submitButton = document.getElementById('submit')
-    submitButton.addEventListener('click', (e) => {
+    const submit = document.getElementById('submit')
+    submit.addEventListener('click', (e) => {
+      console.log('click')
       e.preventDefault()
+      const validMsg = `
+      
+      <p>Votre message a bien été envoyé à <br>${nameOfPhotographerId}</p>
+      <img src="../img/croix.png" alt="fermer la modale de validation d'envoi du message" />
+      <input id="close__msg" type="submit" value="Fermer" />
+      `
+      form.innerHTML = validMsg
+      const closeMsg = document.getElementById('close__msg')
+      const closeMsgCrux = document.querySelector('.formulaire>img')
+      console.log(closeMsg)
+      closeMsg.addEventListener('click', () => {
+        main.removeChild(form)
+        e.preventDefault()
+      })
+      closeMsgCrux.addEventListener('click', () => {
+        main.removeChild(form)
+      })
     })
   })
 }
