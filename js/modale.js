c9f2041ca7690fcf32f5ead24f2088a671dbcf2c
kirmann <k.dkle@hotmail.fr>
2021-09-16 20:49:53 +0200

    ajout de la vérification des inputs

--- js/modale.js
+++ js/modale.js
@@ -7,7 +7,7 @@ export function contactPhotographer (nameOfPhotographerId) {
   contact.addEventListener('click', () => {
     const modale = `    
     <h1>Contactez-moi<br />${nameOfPhotographerId}</h1>
-    <img src="../img/croix.png" alt="fermer la modale de contact" />
+    <img src="../img/croix.png" alt="fermer la modale de contact" />    
     <label for="firstname">Prénom</label>
     <input type="text" name="firstname" id="firstname" required/>
     <span class = "firstname"></span>
@@ -24,32 +24,108 @@ export function contactPhotographer (nameOfPhotographerId) {
     `
     form.innerHTML = modale
     main.appendChild(form)
+    let erreur
+    // INJECTION DES ATTRIBUTS EN CAS DE PROBLEME
+    function setAtt (value) {
+      value.parentElement.setAttribute('data-error', erreur)
+      value.parentElement.setAttribute('data-error-visible', 'true')
+    }
+
+    // SUPRESSION DES ATTRIBUTS
+    function removeAtt (value) {
+      value.parentElement.removeAttribute('data-error')
+      value.parentElement.removeAttribute('data-error-visible')
+    }
+
+    // TEST DE LA LONGUEUR ET DE LA VALIDITÉE DU CHAMP NOM ET PRENOM
+    function testFirstAndLast (input) {
+      return /^[^0-9_!¡?÷?¿/\\+=@#$%ˆ&*(){}|~<>;:[\]]{2,}$/.test(input)
+    }
+    const first = document.getElementById('firstname')
+
+    // FONCTION DE TEST DU PRENOM
+    function testFirstName () {
+      if (testFirstAndLast(first.value.trim())) {
+        removeAtt(first)
+        return true
+      } else {
+        erreur = 'veuillez entrer un prénom de 2 lettres minimum'
+        setAtt(first)
+        return false
+      }
+    }
+
+    // FONCTION DE TEST DU NOM
+    const last = document.getElementById('name')
+
+    function testLastName () {
+      if (testFirstAndLast(last.value.trim())) {
+        removeAtt(last)
+        return true
+      } else {
+        erreur = 'veuillez entrer un nom de 2 lettres minimum'
+        setAtt(last)
+        return false
+      }
+    }
+
+    // TEST DE LA VALIDITE DE L EMAIL
+    function testMail (input) {
+      return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(input.value)
+    }
+
+    // FONCTION DE TEST DU MAIL
+    const mail = document.getElementById('email')
+
+    function email () {
+      if (testMail(mail)) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    const textarea = document.getElementById('message')
+
+    function textArea () {
+      if (testFirstAndLast(textarea.value.trim())) {
+        return true
+      } else {
+        return false
+      }
+    }
+
     // fermeture de la modale au click sur la croix
     const closeModal = document.querySelector('.formulaire>img')
     closeModal.addEventListener('click', () => {
       main.removeChild(form)
     })
+
     const submit = document.getElementById('submit')
     submit.addEventListener('click', (e) => {
       console.log('click')
       e.preventDefault()
-      const validMsg = `
+      if (testFirstName() && testLastName() && email() && textArea()) {
+        console.log('prénom ' + first.value + ', nom ' + last.value + ', email ' + mail.value + ' , message ' + textarea.value)
+        // création de la modale de validation
+        const validMsg = `
       
       <p>Votre message a bien été envoyé à <br>${nameOfPhotographerId}</p>
       <img src="../img/croix.png" alt="fermer la modale de validation d'envoi du message" />
       <input id="close__msg" type="submit" value="Fermer" />
       `
-      form.innerHTML = validMsg
-      const closeMsg = document.getElementById('close__msg')
-      const closeMsgCrux = document.querySelector('.formulaire>img')
-      console.log(closeMsg)
-      closeMsg.addEventListener('click', () => {
-        main.removeChild(form)
-        e.preventDefault()
-      })
-      closeMsgCrux.addEventListener('click', () => {
-        main.removeChild(form)
-      })
+        form.innerHTML = validMsg
+        const closeMsg = document.getElementById('close__msg')
+        const closeMsgCrux = document.querySelector('.formulaire>img')
+        console.log(closeMsg)
+        closeMsg.addEventListener('click', () => {
+          main.removeChild(form)
+          e.preventDefault()
+        })
+        closeMsgCrux.addEventListener('click', () => {
+          main.removeChild(form)
+        })
+      }
     })
   })
 }
